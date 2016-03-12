d903a5f188241005dbb3c62c3877b19008d3a9ef
Charlie Imhoff <cpimhoff@gmail.com>
2016-03-11 21:09:53 -0600

    Index page for posts looks fine and works.

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,15 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  root 'posts#index'
+
   resources :posts do
     member do
       get 'delete'  #delete form not default
     end
   end
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  
+
 end
