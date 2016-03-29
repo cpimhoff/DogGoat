f0d2ae2a6e210e22bdd5f9cce60589ff43b0ee26
Charlie Imhoff <cpimhoff@gmail.com>
2016-03-29 00:07:09 -0500

    Generate spicy claim codes

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,12 +1,10 @@
-require 'bcrypt'
-
 class Invite < ActiveRecord::Base
 
   belongs_to :owner, class_name: "Member", foreign_key: "owner_id"
 
   EMAIL_RX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
-  validates_uniqueness_of :claim_code
+  validates_uniqueness_of :claim_code, message: "claim code is not unique"
   validates_presence_of :claim_code
   validates_presence_of :owner_id, message: "Invites must be sent by a valid member."
 
@@ -14,5 +12,34 @@ class Invite < ActiveRecord::Base
   validates_presence_of :last_name
   validates_format_of :email, with: EMAIL_RX, message: "Please provide a valid email address."
   validates_confirmation_of :email
+
+  def self.generate_new
+    invite = Invite.new
+
+    raw_lang = %w(0 1 2 3 4 5 6 7 8 9)
+    spice_lang = %w(DOG GOAT BIT POP CAT ZIP BUN RED HOP WOW BUTT SUN ZOO 007 TOY FEW APE FROG STAR COW GYM SAO ABE GIN MAX MIN ZAP POW WIG DIP GOD SAW AXE QOC CUJ DEW LIT BOX BOP OBJ CPI DAN
+      DIN RUB BUM GIT DEE NIP XXX)
+
+    generated_code = ""
+    until generated_code.length == 10
+      if rand(2) == 1
+        # add a spicy word if possible
+        spicy_word = spice_lang[rand(spice_lang.count)]
+        if (generated_code.length + spicy_word.length) < 10
+          generated_code += spicy_word
+        end
+      else
+        # add a raw letter
+        raw_letter = raw_lang[rand(raw_lang.count)]
+        generated_code += raw_letter
+      end
+    end
+    puts spice_lang.count
+
+    generated_code.upcase!
+    invite.claim_code = generated_code
+
+    return invite
+  end
 
 end
