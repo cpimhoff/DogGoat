f34ffd97ad6c4b6fa54f48a91c8ed65c2238e6ae
Charlie Imhoff <cpimhoff@gmail.com>
2016-04-14 16:05:35 -0500

    fix to error message

--- app/models/riff.rb
+++ app/models/riff.rb
@@ -6,7 +6,7 @@ class Riff < ActiveRecord::Base
   validates_presence_of :author_id, message: "Riffs must have an authoring member."
   validates_presence_of :content, message: "Riffs need content."
   validates_length_of :content, maximum: 200, message: "This riff is pretty long. Make it short and snappy!"
-  validates_numericality_of :votes, greater_than_or_equal_to: 0, only_integer: true, message: "Prompts can't have fewer than zero votes."
+  validates_numericality_of :votes, greater_than_or_equal_to: 0, only_integer: true, message: "Riffs can't have fewer than zero votes."
 
   scope :by_recent, -> {order('created_at DESC')}
   scope :by_vote, -> {order('votes DESC')}
